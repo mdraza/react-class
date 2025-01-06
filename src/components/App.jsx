0c1ce5d69cf9758f29b71197cb56c74693755202
mdraza <raza.uidev@gmail.com>
2025-01-06 20:36:31 +0530

    hook & counter

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,18 +1,58 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import User from "./User";
 import Student from "./Student";
 import Employee from "./Employee";
 import NewStudent from "./NewStudent";
 import Post from "./Post";
 import StateUse from "./StateUse";
+import Counter from "./Counter";
 
 function App() {
+  const [post, setPost] = useState([]);
+
+  // Normal Function
+  // Demo();
+  // function Demo() {
+  //   console.log(123);
+  // }
+
+  //Arrow Function
+  // const ArrowFun = () => {
+  //   console.log("Arrow Function");
+  // };
+  // ArrowFun();
+
+  // useEffect(function () {});
+
+  // useEffect(() => {
+  //   console.log("useEffect rendered");
+  // }, []);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch(
+        "https://jsonplaceholder.typicode.com/users"
+      );
+      const result = await response.json();
+      console.log(result);
+      setPost(result);
+    };
+    fetchData();
+  }, []);
+
+  console.log("App Rendered");
+
   return (
     <div>
       {/* <User data={arrObj} /> */}
       {/* <NewStudent /> */}
       {/* <Post /> */}
-      <StateUse />
+      {/* <StateUse /> */}
+      <Counter />
+
+      {post.map((post) => (
+        <p>{post.name}</p>
+      ))}
     </div>
   );
 }
